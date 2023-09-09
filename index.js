9097a5cbd36907d4047725e3de813e8b8fedbed3
Kvi666 <kavisl.fantasy@gmail.com>
2023-09-09 21:01:02 +0530

    #

---
+++ index.js
@@ -0,0 +1,28 @@
+// Sticky Navbar
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 45) {
+    $(".navbar").addClass("sticky-top shadow-sm");
+  } else {
+    $(".navbar").removeClass("sticky-top shadow-sm");
+  }
+});
+
+// on screen
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
